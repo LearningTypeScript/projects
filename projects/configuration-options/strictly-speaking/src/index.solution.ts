a7c14b9dfec3b033ef72eeb6acb50415b4b640ac
Josh Goldberg <me@joshuakgoldberg.com>
2022-06-07 11:16:10 -0400

    ✨ feat: add Configuration Options > Strictly Speaking entree project (#85)

---
+++ projects/configuration-options/strictly-speaking/src/index.solution.ts
@@ -0,0 +1,63 @@
+// Add your type annotations here! ✨
+
+import { words } from "./words";
+
+async function seconds(amount: number) {
+	await new Promise((resolve) => setTimeout(resolve, amount * 1000));
+}
+
+async function speakLines(lines: (number | string | undefined)[]) {
+	for (const line of lines) {
+		if (typeof line === "string") {
+			console.log(line);
+		} else {
+			console.log("...");
+			await seconds(line ?? 1);
+		}
+	}
+}
+
+function generateLines(quantity: number) {
+	const lines: (number | string | undefined)[] = [generateLine("Lorem ipsum")];
+
+	for (let i = 0; i < quantity - 1; i += 1) {
+		lines.push(generateLine());
+
+		if (randomInt(0, 7) >= 4) {
+			lines.push(i % 2 === 0 ? undefined : randomInt(4));
+		}
+	}
+
+	return lines;
+}
+
+function generateLine(prefix?: string) {
+	const words: string[] = prefix ? [prefix] : [];
+	const quantity = randomInt(3, 10);
+
+	for (let i = 0; i < quantity; i += 1) {
+		words.push(randomWord());
+	}
+
+	return `${words.join(" ")}.`;
+}
+
+function randomInt(to: number): number;
+function randomInt(from: number, to: number): number;
+function randomInt(fromOrTo: number, to?: number): number {
+	return to === undefined
+		? randomInt(0, fromOrTo)
+		: Math.floor(Math.random() * to) + fromOrTo;
+}
+
+function randomWord() {
+	return words[randomInt(0, words.length)];
+}
+
+async function makeGrandSpeech() {
+	const lines = generateLines(randomInt(5, 10));
+
+	await speakLines(lines);
+}
+
+makeGrandSpeech();
