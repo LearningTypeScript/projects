61860d8db71952917c1bf7476c8e93adfe1f3d56
Josh Goldberg <git@joshuakgoldberg.com>
2022-08-04 11:38:13 -0400

    Progress: chapter file verification

--- test/files.test.ts
+++ test/files.test.ts
@@ -1,31 +1,67 @@
 import { describe, expect, it, test } from "@jest/globals";
 import * as fs from "fs";
 
-const projects = fs.readdirSync("projects").flatMap((category) => {
-	return fs
-		.readdirSync(`projects/${category}`)
-		.filter((fileName) => !fileName.includes("."))
-		.map((project) => `${category}/${project}`);
-});
+const chapters = fs.readdirSync("projects");
 
-describe("Project Files", () => {
-	for (const project of projects) {
-		describe(project, () => {
-			const contents = fs.readdirSync(`projects/${project}`);
-			const testGenerator = contents.includes("src")
-				? testEntreeOrDessert
-				: testAppetizer;
+for (const chapter of chapters) {
+	describe(chapter, () => {
+		const chapterDirectory = `projects/${chapter}`;
+		const chapterContents = fs.readdirSync(chapterDirectory);
 
-			testGenerator(project, contents);
+		test("_category_.json", () => {
+			const categoryContents = fs
+				.readFileSync(`${chapterDirectory}/_category_.json`)
+				.toString();
+			const categoryData = JSON.parse(categoryContents);
+
+			expect(categoryData).toEqual({
+				label: expect.stringMatching(
+					new RegExp(chapter.replaceAll("-", " "), "i")
+				),
+				position: expect.any(Number),
+			});
 		});
-	}
-});
+
+		test("README.json", () => {
+			const readmeContents = fs
+				.readFileSync(`${chapterDirectory}/README.md`)
+				.toString();
+
+			expect(readmeContents).toEqual(
+				expect.stringMatching(
+					new RegExp(`^# ${chapter.replaceAll("-", " ")}`, "i")
+				)
+			);
+		});
+
+		const projects = fs
+			.readdirSync(`projects/${chapter}`)
+			.filter((fileName) => !fileName.includes("."));
+
+		for (const project of projects) {
+			describe(project, () => {
+				const projectContents = fs.readdirSync(
+					`projects/${chapter}/${project}`
+				);
+				const testGenerator = projectContents.includes("src")
+					? testEntreeOrDessert
+					: testAppetizer;
+
+				testGenerator(project, projectContents);
+			});
+		}
+	});
+}
 
 function testAppetizer(_project: string, contents: string[]) {
 	it("contains the minimum expected files", () => {
-		expect(contents).toContain("README.md");
 		expect(contents).toContain("_category_.json");
 		expect(contents).toContain("package.json");
+		expect(contents).toContain("README.md");
+	});
+
+	test("_category_.json", () => {
+		//
 	});
 }
 
