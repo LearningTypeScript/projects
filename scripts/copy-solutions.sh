8d189801c767bf6aced84835c4cf17a6fbf926ac
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-19 14:57:12 -0400

    Revert unrelated fix

--- scripts/copy-solutions.sh
+++ scripts/copy-solutions.sh
@@ -2,9 +2,6 @@
 # Projects that use classes with # private members would otherwise report assignability errors.
 find projects -type f -name "*.test.*" -print0 | xargs -0 sed -i '' -e 's/\? solution/\? index/g'
 
-# Similarly, "index as unknown as typeof solution" type assertions are used for class #privates.
-find projects -type f -name "*.test.*" -print0 | xargs -0 sed -i '' -e 's/as typeof solution/as typeof index/g'
-
 for solutionPath in $(find projects -name \*solution\*); do
 	if [[ $solutionPath == */solution.* ]]; then
 		replacedPath=${solutionPath//"solution"/"index"}
