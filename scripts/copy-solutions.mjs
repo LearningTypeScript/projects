6171ba2dea760b9e694858fb6f56589545a06f50
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-25 16:53:11 -0400

    fix: convert copy-solutions script to .mjs

---
+++ scripts/copy-solutions.mjs
@@ -0,0 +1,30 @@
+import chalk from "chalk";
+import fs from "fs/promises";
+import { glob } from "glob";
+
+console.log(chalk.blue("Starting"), chalk.cyanBright("copy-solutions.mjs"));
+
+for (const testFile of await glob("projects/**/*.test.*")) {
+	console.log(chalk.gray("Replacing solution with index in", testFile));
+	await fs.writeFile(
+		testFile,
+		(await fs.readFile(testFile))
+			.toString()
+			.replaceAll("? solution", "? index")
+			.replaceAll(" as typeof solution", " as typeof index ")
+	);
+}
+
+for (const solutionPath of await glob("projects/**/*solution*")) {
+	const replacementPath = solutionPath.includes("solution.")
+		? solutionPath.replace("solution", "index")
+		: solutionPath.replace(".solution", "");
+
+	if (solutionPath === replacementPath) {
+		console.log(
+			chalk.gray("Skipping", solutionPath, "(no replacement found).")
+		);
+	} else {
+		console.log(chalk.gray("Copying", solutionPath, "to", replacementPath));
+	}
+}
