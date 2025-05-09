bdb7913fe0b63158982da2e3fb2c78b1b145ecb6
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-05-08 21:45:54 -0400

    fix: correct reversed types in 03-split-on (#291)
    * fix: correct reversed types in 03-split-on
    
    * fix: correct reversed types in 03-split-on

--- projects/type-operations/template-literal-type-shenanigans/03-split-on/solution.ts
+++ projects/type-operations/template-literal-type-shenanigans/03-split-on/solution.ts
@@ -3,5 +3,5 @@ export type SplitOn<
 	On extends string,
 	Results extends string[] = [],
 > = Text extends `${infer Prefix}${On}${infer Suffix}`
-	? SplitOn<Suffix, On, [Prefix, ...Results]>
-	: [Text, ...Results];
+	? SplitOn<Suffix, On, [...Results, Prefix]>
+	: [...Results, Text];
